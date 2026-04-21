7fd46f273c19f9b693ef4eb4c85ccdc07c29c0de
Khaled Jalal Al-arashi <k.j.alarashi@gmail.com>
2026-04-21 15:25:06 +0300

    Add files via upload

---
+++ Lab07/src/lab07/Arrays_2D.java
@@ -0,0 +1,78 @@
+package lab07;
+
+import java.util.Scanner;
+
+public class Arrays_2D {
+
+    public static void printArr(int[][] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print("arr[" + row + "][" + col + "] = " + arr[row][col] + "     ");
+            }
+            System.out.println();
+        }
+        System.out.println("============================");
+    }
+
+    public static void inputArrElements(int[][] arr) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Elements : ");
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = input.nextInt();
+            }
+
+        }
+    }
+
+    public static int[][] mulTwoArrays(int[][] arr1, int[][] arr2) {
+        if (arr1[0].length != arr2.length) {
+            return null;
+        }
+
+        int[][] res = new int[arr1.length][arr2[0].length];
+        for (int row = 0; row < res.length; row++) {
+            for (int col = 0; col < res.length; col++) {
+                res[row][col] = arr1[row][col] * arr2[row][col];
+            }
+        }
+        return res;
+    }
+
+    public static int[][] sumTwoArrays(int[][] arr1, int[][] arr2) {
+        if (arr1.length != arr2.length || arr1[0].length != arr2[0].length) {
+            return null;
+        }
+
+        int[][] res = new int[arr1.length][arr1[0].length];
+        for (int row = 0; row < res.length; row++) {
+            for (int col = 0; col < res.length; col++) {
+                res[row][col] = arr1[row][col] + arr2[row][col];
+            }
+        }
+        return res;
+    }
+
+    public static boolean search(int[][] arr, int s) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr.length; col++) {
+                if (arr[row][col] == s) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int[] copyTo1DArray(int[][] arr) {
+        int[] res = new int[arr.length * arr[0].length];
+        int i = 0;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
+                res[i] = arr[row][col];
+                i++;
+            }
+        }
+            return res;
+    }
+}
