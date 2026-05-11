d6eb3d91a0097ebe855195aed3a19819ec51ee88
Khaled Jalal Al-arashi <k.j.alarashi@gmail.com>
2026-05-11 20:27:13 +0300

    Add files via upload

---
+++ SimpleProject/src/classes/Main.java
@@ -0,0 +1,12 @@
+package classes;
+
+import GUI.Login;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+    }
+
+}
