7ff879585a44fff5dc0dcfeed7a8e7a34a0b4b0e
Khaled Jalal Al-arashi <k.j.alarashi@gmail.com>
2026-07-23 22:28:33 +0300

    Add Arrays class with various array operations

---
+++ Lab04/src/lab04/Arrays.java
@@ -0,0 +1,108 @@
+package lab04;
+
+import java.util.Scanner;
+
+public class Arrays {
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("arr[" + i + "] = " + arr[i] + "     ");
+        }
+        System.out.println("\n=============================");
+    }
+
+    public static void inputArr(int[] arr) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Array Elements : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
+       System.out.println("\n=============================");
+    }
+
+    public static boolean search(int[] arr, int s) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean updateByElement(int[] arr, int oldElement, int newElement) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == oldElement) {
+                arr[i] = newElement;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean updateByIndex(int[] arr, int index, int newElement) {
+        if (index < arr.length) {
+            arr[index] = newElement;
+            return true;
+        }
+            return false;
+    }
+
+    // دالة الحذف من دون عملية (shifting) 
+    public static boolean delete(int[] arr, int oldElement) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == oldElement) {
+                arr[i] = 0;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] sumTwoArrays(int[] arr1, int[] arr2) {
+        // شرط للتاكد من ان طول المصفوفتين متساويتين
+        if (arr1.length == arr2.length) {
+            int[] res = new int[arr1.length];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = arr1[i] + arr2[i];
+            }
+            return res;
+        }
+        return null;
+    }
+
+    public static int[] mulTwoArrays(int[] arr1, int[] arr2) {
+        // شرط للتاكد من ان طول المصفوفتين متساويتين
+        if (arr1.length == arr2.length) {
+            int[] res = new int[arr1.length];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = arr1[i] * arr2[i];
+            }
+            return res;
+        }
+        return null;
+    }
+
+    public static int maxElement(int[] arr) {
+        // شرط التأكد من ان المصفوفة غير فارغة
+        // يتم طباعة عبارة وارجاع -1 
+        // -1 ليس رقم في المصفوفة وانما يدل على ان المصفوفة فارغة
+        if (arr.length == 0) {
+            System.out.println("The Array is empty ...");
+            return -1;
+        }
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void deleteWithShift(int[] arr, int index) {
+        for (int i = index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = 0;
+    }
+}
