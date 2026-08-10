deba2575090815d2f4f22a535eaf412023244024
Khaled Jalal Al-arashi <k.j.alarashi@gmail.com>
2026-08-10 23:57:52 +0300

    Update BankAccount.java

--- Lab07/src/lab07/BankAccount.java
+++ Lab07/src/lab07/BankAccount.java
@@ -3,18 +3,18 @@ package lab07;
 public class BankAccount {
 
     private int accountId;
-    private String accountHolder;
+    private String accountholder;
     private double amount;
 
-    public BankAccount(int accountId, String accountHolder) {
+    public BankAccount(int accountId, String accountholder) {
         this.accountId = accountId;
-        this.accountHolder = accountHolder;
+        this.accountholder = accountholder;
         amount = 0.0;
     }
 
-    public BankAccount(int accountId, String accountHolder, double amount) {
+    public BankAccount(int accountId, String accountholder, double amount) {
         this.accountId = accountId;
-        this.accountHolder = accountHolder;
+        this.accountholder = accountholder;
         this.amount = amount;
     }
 
@@ -22,12 +22,12 @@ public class BankAccount {
         return accountId;
     }
 
-    public String getAccountHolder() {
-        return accountHolder;
+    public String getAccountholder() {
+        return accountholder;
     }
 
-    public void setAccountHolder(String accountHolder) {
-        this.accountHolder = accountHolder;
+    public void setAccountholder(String accountholder) {
+        this.accountholder = accountholder;
     }
 
     public double getAmount() {
@@ -35,31 +35,32 @@ public class BankAccount {
     }
 
     public void deposit(double amount) {
-        this.amount = this.amount + amount;
-        System.out.println("====================================");
-        System.out.println("Deposit Amount  :  " + amount+" $ ");
-        System.out.println("Total Amount    :  " + getAmount()+" $ ");
-        System.out.println("====================================");
+        if (amount > 0) {
+            this.amount = this.amount + amount;
+            System.out.println("====================================");
+            System.out.println("Deposit Amount  :  " + amount + " $ ");
+            System.out.println("Total Amount    :  " + getAmount() + " $ ");
+            System.out.println("====================================");
+        }
     }
 
     public void withdraw(double amount) {
         if (amount <= this.amount && this.amount > 0) {
             this.amount = this.amount - amount;
             System.out.println("====================================");
-            System.out.println("Withdraw Amount :  " + amount+" $ ");
-            System.out.println("Total Amount    :  " + getAmount()+" $ ");
+            System.out.println("Withdraw Amount :  " + amount + " $ ");
+            System.out.println("Total Amount    :  " + getAmount() + " $ ");
             System.out.println("====================================");
-        }
-        else {
+        } else {
             System.out.println("Sorry , You Don't Have Enough Cash ...!");
         }
     }
-    
-    public void display(){
+
+    public void display() {
         System.out.println("====================================");
-        System.out.println("ID              :   "+getAccountId());
-        System.out.println("NAME            :   "+getAccountHolder());
-        System.out.println("Current Amount  :   "+getAmount()+" $ ");
+        System.out.println("ID              :   " + getAccountId());
+        System.out.println("NAME            :   " + getAccountholder());
+        System.out.println("Current Amount  :   " + getAmount() + " $ ");
         System.out.println("====================================");
     }
 
